9f7553834b062906b4d0354548dea4980c78ff69
Arman Kuzembaev <armansk8er@gmail.com>
2024-03-25 18:49:20 +0500

    s

--- src/components/balance/balance.jsx
+++ src/components/balance/balance.jsx
@@ -6,12 +6,14 @@ const Balance = () => {
 	const counter = 0.1524;
 
 	return (
-		<div className={'balance'}>
-			<h4 className='balance__title'>AVCOIN balance</h4>
-			<h1 className='balance__counter'>{counter}</h1>
-			<img className='balance__img' src={balanceSticker} alt='Balance'/>
-			<button className='swop__button'>ОБМЕНЯТЬ</button>
-			<button className='swop__button'>ЗАРАБОТАТЬ</button>
+		<div>
+			<div className={'balance'}>
+				<h4 className='balance__title'>AVCOIN balance</h4>
+				<h1 className='balance__counter'>{counter}</h1>
+				<img className='balance__img' src={balanceSticker} alt='Balance'/>
+				<button className='swop__button'>ОБМЕНЯТЬ</button>
+				<button className='swop__button'>ЗАРАБОТАТЬ</button>
+			</div>
 		</div>
 	);
 };
