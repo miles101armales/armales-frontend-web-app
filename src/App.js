569d565042e71feb015db4fa6b91e11d88a7752d
Arman Kuzembaev <armansk8er@gmail.com>
2024-03-25 16:12:17 +0500

    s

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
+import 'telegram-web-app';
+
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+  useEffect( () => {
+    tg.ready();
+  }, [])
+
   return (
     <div className="App">
       work
